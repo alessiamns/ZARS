3b93a2f6d5d4925114e4f902dd4a73b8ad156db9
Alessia Minissale <minissalealessia@gmail.com>
2020-05-07 18:52:46 +0200

    Update Hotel_Info.py

--- Hotel_Info.py
+++ Hotel_Info.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.chrome.options import 
+from selenium.webdriver.chrome.options import Options
 from http import cookies
 import time
 import re
